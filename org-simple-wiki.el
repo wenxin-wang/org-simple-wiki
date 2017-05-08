5f611af2ff055f1e85983bcc44a7182194a3f1d3
Wenxin Wang <wenxin.wang94@gmail.com>
2017-05-08 15:54:17 +0800

    add keyword list

--- org-simple-wiki.el
+++ org-simple-wiki.el
@@ -5,6 +5,7 @@
 ;; This seems not a package but more like a bunch of helper functions.
 ;; I do not think it suits general usage, but feel free to try and change it.
 
+(require 'cl-lib)
 (require 'helm-projectile)
 (require 'helm-ag)
 
@@ -18,6 +19,26 @@ Default value ~/org/wiki."
   :type 'directory
   :group 'org-simple-wiki)
 
+(defconst org-simple-wiki--keyword "wiki_kw"
+  "The org keyword for wiki keyword, as in #+WIKI_KW: keyword1 keyword2")
+
+(defun org-simple-wiki-search-ag ()
+  "Search pages"
+  (interactive)
+  (helm-do-ag org-simple-wiki-location))
+
+(defun org-simple-wiki-search-keyword-ag (&optional word)
+  "Search pages by keywords"
+  (interactive)
+  ;; Insert word at point for keyword
+  (setq word (if word (downcase word)
+               (setq word (or (symbol-at-point) ""))))
+  (with-temp-buffer ; Dirty trick for helm-ag to use customized default input
+    (insert (format "^[ \\t]*#\\+%s: %s" org-simple-wiki--keyword word))
+    (let ((helm-ag--extra-options "-G\\.org$")
+          (helm-ag-insert-at-point 'paragraph))
+      (helm-do-ag org-simple-wiki-location))))
+
 (defun org-simple-wiki-find-file ()
   "Open files in the default wiki"
   (interactive)
@@ -26,21 +47,25 @@ Default value ~/org/wiki."
         (cd org-simple-wiki-location)
         (helm-projectile-find-file)
         (cd cwd))
-      (message "`%s' is not accessible as a directory" org-simple-wiki-location)))
+    (message "`%s' is not accessible as a directory" org-simple-wiki-location)))
 
-(defun org-simple-wiki-search-ag ()
-  "Search pages"
-  (interactive)
-  (helm-do-ag org-simple-wiki-location))
+(defun org-simple-wiki--list-keywords (dir)
+  "List all keywords in a wiki"
+  (with-temp-buffer
+    (call-process "grep" nil t nil "-r" "-h" "-o" "-P"
+                  (format "(?<=#\\+%s:).*"
+                          (upcase org-simple-wiki--keyword))
+                  (expand-file-name dir))
+    (remove-duplicates (split-string (buffer-string))
+                       :test 'string=)))
 
-(defun org-simple-wiki-search-keyword-ag ()
-  "Search pages by WIKI_KW"
+(defun org-simple-wiki-find-file-by-keyword ()
+  "Find page by existing keywords"
   (interactive)
-  (let ((word (or (symbol-at-point) ""))) ; Insert word at point for keyword
-    (with-temp-buffer ; Dirty trick for helm-ag to use customized default input
-      (insert (format "^[ \\t]*#\\+wiki_kw %s" word))
-      (let ((helm-ag--extra-options "-G\\.org$")
-            (helm-ag-insert-at-point 'paragraph))
-        (helm-do-ag org-simple-wiki-location)))))
+  (helm :sources
+        `((name . "A list of wiki keywords")
+          (candidates . ,(org-simple-wiki--list-keywords
+                          org-simple-wiki-location))
+          (action . org-simple-wiki-search-keyword-ag))))
 
 (provide 'org-simple-wiki)
